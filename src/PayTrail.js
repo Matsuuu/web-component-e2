7d8b76faf6a7f5877979e99eb9e819d3e746a54a
Matsuuu <huhta.matias@gmail.com>
2020-07-20 20:17:04 +0300

    Field generation and started authcode generation

--- src/PayTrail.js
+++ src/PayTrail.js
@@ -1,24 +1,26 @@
+const paymentUrl = 'https://payment.paytrail.com/e2';
+
 const template = document.createElement('template');
 template.innerHTML = `
+<form action=${paymentUrl} method="POST">
 
+</form>
 `;
 
 const requiredFields = [
-    'MERCHANT_ID',
-    'URL_SUCCESS',
-    'URL_CANCEL',
-    'ORDER_NUMBER',
-    'AMOUNT',
-    'PARAMS_IN',
-    'PARAMS_OUT',
-    'AUTHCODE',
+    'merchant_id',
+    'url_success',
+    'url_cancel',
+    'order_number',
+    'amount',
+    'params_in',
+    'params_out',
+    'authcode',
 ];
 
-const requiredProductFields = [
-    'ITEM_TITLE',
-    'ITEM_UNIT_PRICE',
-    'ITEM_VAT_PERCENT',
-];
+const requiredParamsOut = ['payment_id', 'timestamp', 'status'];
+
+const requiredProductFields = ['item_title', 'item_unit_price', 'item_vat_percent'];
 
 export class PayTrail extends HTMLElement {
     constructor() {
@@ -27,74 +29,117 @@ export class PayTrail extends HTMLElement {
         this.initializeFields();
         const root = this.attachShadow({ mode: 'open' });
         root.appendChild(template.content.cloneNode(true));
-    }
-
-    att(attributeName) {
-        return this.hasAttribute(attributeName)
-            ? this.getAttribute(attributeName)
-            : '';
+        document.addEventListener('click', this.calculateAuthCode.bind(this));
     }
 
     initializeFields() {
-        this.MERCHANT_ID = '13466';
-        this.CURRENCY = 'EUR';
-        this.AMOUNT = 0.0;
-        this.ALG = 1;
-        this.LOCALE = 'fi_FI';
-        this.VAT_IS_INCLUDED = 1;
+        this.merchant_authentication_hash = '6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ';
+        this.submit_button_label = 'Pay here';
+        this.merchant_id = '13466';
+        this.currency = 'EUR';
+        this.amount = 0.0;
+        this.alg = 1;
+        this.locale = 'fi_FI';
+        this.vat_is_included = 1;
     }
+
+    calculateAuthCode() {
+        const authCodeString = Array.from(this.shadowRoot.querySelectorAll('input[type=hidden]'))
+            .filter(f => f.name !== 'AUTHCODE')
+            .reduce((a, b) => {
+                return `${a}|${b.value}`;
+            }, '');
+        console.log(authCodeString);
+        // TODO: Figure out how to hash it without deps. Might have to use deps tho.
+    }
+
+    calculatePaymentId() {}
 
     static get observedAttributes() {
         return [
-            'MERCHANT_ID',
-            'URL_SUCCESS',
-            'URL_CANCEL',
-            'ORDER_NUMBER',
-            'AMOUNT',
-            'PARAMS_IN',
-            'ALG',
-            'AUTHCODE',
-            'URL_NOTIFY',
-            'LOCALE',
-            'REFERENCE_NUMBER',
-            'PAYMENT_METHODS',
-            'VAT_IS_INCLUDED',
-            'MSG_SETTLEMENT_PAYER',
-            'MSG_UI_PAYMENT_METHOD',
-            'MSG_UI_MERCHANT_PANEL',
-            'EXPIRATION_FOR_PAYMENT_CREATION',
-            'PAYER_PERSON_FIRSTNAME',
-            'PAYER_PERSON_LASTNAME',
-            'PAYER_PERSON_EMAIL',
-            'PAYER_PERSON_PHONE',
-            'PAYER_PERSON_ADDR_STREET',
-            'PAYER_PERSON_ADDR_POSTAL_CODE',
-            'PAYER_PERSON_ADDR_TOWN',
-            'PAYER_PERSON_ADDR_COUNTRY',
-            'PAYER_COMPANY_NAME',
+            'submit_button_label',
+            'merchant_authentication_hash',
+            'merchant_id',
+            'url_success',
+            'url_cancel',
+            'order_number',
+            'amount',
+            'params_in',
+            'params_out',
+            'alg',
+            'authcode',
+            'url_notify',
+            'locale',
+            'reference_number',
+            'payment_methods',
+            'vat_is_included',
+            'msg_settlement_payer',
+            'msg_ui_payment_method',
+            'msg_ui_merchant_panel',
+            'expiration_for_payment_creation',
+            'payer_person_firstname',
+            'payer_person_lastname',
+            'payer_person_email',
+            'payer_person_phone',
+            'payer_person_addr_street',
+            'payer_person_addr_postal_code',
+            'payer_person_addr_town',
+            'payer_person_addr_country',
+            'payer_company_name',
         ];
     }
 
     attributeChangedCallback(attributeName, oldValue, newValue) {
         this._generateFields();
+        this[attributeName] = newValue;
     }
 
     connectedCallback() {
+        this._checkRequiredFields();
         this._generateFields();
     }
 
+    _checkRequiredFields() {
+        const splitParamsOut = this.params_out ? this.params_out.toLowerCase().split(',') : [];
+        requiredParamsOut.forEach(paramOut => {
+            if (!splitParamsOut.includes(paramOut.toLowerCase())) {
+                throw Error(
+                    `PARAMS_OUT doesn't include all of the required fields.\nRequired fields:\n\n${requiredParamsOut.map(
+                        pout => `${pout.toUpperCase()}\n`
+                    )}`
+                );
+            }
+        });
+    }
+
     _generateFields() {
-        this.shadowRoot.innerHTML = '';
-        const fields = this.att('PARAMS_IN').split(',');
+        console.log({ ...this });
+
+        const form = this.shadowRoot.querySelector('form');
+        form.innerHTML = '';
+        const fields = this.PARAMS_IN ? this.PARAMS_IN.split(',') : [];
+
+        requiredFields.forEach(reqField => {
+            if (!fields.includes(reqField)) {
+                fields.push(reqField);
+            }
+        });
         const documentFragment = document.createDocumentFragment();
+
         fields.forEach(field => {
             const input = document.createElement('input');
             input.type = 'hidden';
-            input.name = field;
-            input.value = this.att(field);
+            input.name = field.toUpperCase();
+            input.value = this[field] || '';
             documentFragment.appendChild(input);
         });
-        this.shadowRoot.appendChild(documentFragment);
+
+        const submitButton = document.createElement('input');
+        submitButton.type = 'submit';
+        submitButton.value = this.submit_button_label;
+        documentFragment.appendChild(submitButton);
+
+        form.appendChild(documentFragment);
     }
 }
 
